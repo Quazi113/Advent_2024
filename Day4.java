aadb98cf34ca875d36f368904ed6a4c49c494911
Quazi113 <146048055+Quazi113@users.noreply.github.com>
2024-12-17 14:07:00 -0500

    Day4.java
    Check DownLeft not working number 8

--- Day4.java
+++ Day4.java
@@ -23,15 +23,35 @@ public class Day4 {
                 if (puzzle[r][c].equals("X")) {
                     if (checkForward(r,c,puzzle)) {
                         correct++;
+                        System.out.println("1");
                     }
                     if (checkDown(r,c,puzzle)) {
                         correct++;
+                        System.out.println("2");
                     }
                     if (checkBack(r,c,puzzle)) {
                         correct++;
+                        System.out.println("3");
                     }
                     if (checkUp(r,c,puzzle)) {
                         correct++;
+                        System.out.println("4");
+                    }
+                    if (checkUpRight(r,c,puzzle)) {
+                        correct++;
+                        System.out.println("5");
+                    }
+                    if (checkUpLeft(r,c,puzzle)) {
+                        correct++;
+                        System.out.println("6");
+                    }
+                    if (checkDownRight(r,c,puzzle)) {
+                        correct++;
+                        System.out.println("7");
+                    }
+                    if (checkDownLeft(r,c,puzzle)) {
+                        correct++;
+                        System.out.println("8");
                     }
                 }
             }
@@ -43,7 +63,7 @@ public class Day4 {
     public static Boolean checkForward(int r, int c, String[][] data) {
         if (data[c].length > c + 3) {
             if (data[r][c + 1].equals("M") && data[r][c + 2].equals("A") && data[r][c + 3].equals("S")) {
-               return true;
+                return true;
             }
         }
         return false;
@@ -70,6 +90,42 @@ public class Day4 {
     public static Boolean checkUp(int r, int c, String[][] data) {
         if (r - 3 > 0) {
             if (data[r - 1][c].equals("M") && data[r - 2][c].equals("A") && data[r - 3][c].equals("S")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Boolean checkUpRight(int r, int c, String[][] data) {
+        if ((r - 3) > 0 && data[c].length > (c + 3)) {
+            if (data[r - 1][c + 1].equals("M") && data[r - 2][c + 2].equals("A") && data[r - 3][c + 3].equals("S")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Boolean checkDownRight(int r, int c, String[][] data) {
+        if (data[c].length > (c + 3) && data.length > (r + 3)) {
+            if (data[r + 1][c + 1].equals("M") && data[r + 2][c + 2].equals("A") && data[r + 3][c + 3].equals("S")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Boolean checkUpLeft(int r, int c, String[][] data) {
+        if ((r - 3) > 0 && (c - 3) > 0) {
+            if (data[r - 1][c - 1].equals("M") && data[r - 2][c - 2].equals("A") && data[r - 3][c - 3].equals("S")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Boolean checkDownLeft(int r, int c, String[][] data) {
+        if (c - 3 > 0 && data.length > (r + 3)) {
+            if (data[r + 1][c - 1].equals("M") && data[r + 2][c - 2].equals("A") && data[r + 3][c - 3].equals("S")) {
                 return true;
             }
         }
